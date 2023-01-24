d1a31c10c5e4835c646560f3a09794d0e8390c14
saehim <saeilee.vxv@gmail.com>
2023-01-24 13:31:29 +0900

    Feat (ch2) : 문자열 분리 후 변환 함수, 숫자 덧셈 함수, 음수 예외처리 함수

--- chapter2/string_calculator/StringCalculator.java
+++ chapter2/string_calculator/StringCalculator.java
@@ -1,5 +1,37 @@
 package chapter2.string_calculator;
 
-public class StringCalculator {
+import java.util.Arrays;
 
+public class StringCalculator {
+    /**
+     * 문자열 기본 구분자로 분리하여 숫자 변환
+     * pram : String
+     * return : int[]
+     * */
+    public int[] getSplitedInts(String str) {
+        String[] splitedStr = str.split("[,|:]");
+        int[] result = new int[splitedStr.length];
+        int idx = 0;
+
+        for (String numberStr : splitedStr) {
+            if (isBiggerThan0(Integer.parseInt(numberStr))) {
+                result[idx++] = Integer.parseInt(numberStr);
+            } else if (numberStr.charAt(0)=='-'){
+                throw new RuntimeException("음수입력");
+            }
+        }
+
+        return Arrays.copyOfRange(result, 0,idx);
+    }
+
+    private boolean isBiggerThan0(int num) {
+        if (num<0) {
+            throw new RuntimeException("음수 입력입니다.");
+        }
+        return num >= 0;
+    }
+
+    public int addAll(int[] ints) {
+        return Arrays.stream(ints).sum();
+    }
 }
